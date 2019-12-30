8b92cc6d911cd163d2cef1cb4464323d468fd467
David Moreno <dmoreno788@gmail.com>
2019-12-30 12:02:30 +0100

    Iniciado proyecto y creado servicio

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from "../service/news.service";
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+news : object[];
+  constructor(public newService : NewsService) { }
 
-  constructor() {}
-
+ngOnInit(){
+  this.newService.getNews().subscribe(value=>{
+    this.news = value;
+    console.log(this.news)
+  })
+}
 }
---
+++ src/app/service/news.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  constructor(private http : HttpClient) { }
+
+  getNews():Observable<any>{
+    return this.http.get(`https://www.circuitricardotormo.com/wp-json/wp/v2/posts?page=1&per_page=15&_embed`);
+  }
+}
