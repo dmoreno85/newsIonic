2e6549c6f20b4d2f7de8545f0bcee7e7e7b79047
David Moreno <dmoreno788@gmail.com>
2019-12-30 22:32:41 +0100

    Avatar en home funcionando

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { NewsService } from "../service/news.service";
-import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
-news : object[];
-newIMG :any;
-file : any;
-  constructor(public newService : NewsService,private router : Router ){ }
+export class HomePage implements OnInit {
+  news: object[];
+  constructor(public newService: NewsService) { }
 
-ngOnInit(){
-  this.newService.getNews().subscribe(value=>{
-    this.news = value;
-    this.newIMG = this.news[0]['_embedded']['wp:featuredmedia'][0]['media_details']['file'];
-    console.log(this.news)
+  ngOnInit() {
+    this.newService.getNews().subscribe(value => {
+      this.news = value;
+      console.log(this.news)
+    })
+  };
 
-  })
-}
+
 }
--- src/app/home/news-detail/news-detail.page.ts
+++ src/app/home/news-detail/news-detail.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {NewsService} from '../../service/news.service'
+import { NewsService } from '../../service/news.service'
 import { ActivatedRoute } from "@angular/router";
 @Component({
   selector: 'app-news-detail',
@@ -7,24 +7,17 @@ import { ActivatedRoute } from "@angular/router";
   styleUrls: ['./news-detail.page.scss'],
 })
 export class NewsDetailPage implements OnInit {
-newsValue:object[];
-content : object;
-  constructor(private newService : NewsService, private route : ActivatedRoute) { }
+  newsValue: object[];
+  constructor(private newService: NewsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.route.params.subscribe(params =>{
-      this.newService.getNewId(params.id).subscribe(value=>{
+    this.route.params.subscribe(params => {
+      this.newService.getNewId(params.id).subscribe(value => {
         this.newsValue = value;
-        this.content = value.content.rendered;
-        console.log(this.newsValue)
+        // console.log(this.newsValue)
       })
     })
-    // this.route.paramMap.subscribe(paramMap => {
-    //   const recipeId = paramMap.get('id')
-    //   // console.log(recipeId)
-    //   this.newsValue = this.newService.getNewId(recipeId)
-    //   console.log('hey'+ this.newsValue)
-    // })
+
   }
 
 }
